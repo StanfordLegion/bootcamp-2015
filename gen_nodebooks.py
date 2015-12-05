250b0f9bc923c09e3b284aed52af75f8baac4560
Elliott Slaughter <slaughter@cs.stanford.edu>
2015-12-04 23:34:48 -0800

    Include helpers in notebook.

--- gen_nodebooks.py
+++ gen_nodebooks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import errno, json, os
+import errno, json, os, shutil, subprocess, sys
 
 def make_markdown_cell(source):
     return {
@@ -51,24 +51,35 @@ def make_session_notebook(source_dir, notebook_path):
     notebook = make_notebook([instructions, syntax, circuit])
     with open(notebook_path, 'wb') as f: json.dump(notebook, f, indent=1)
 
-def make_all_sessions(source_dir, notebook_dir):
-    try:
-        os.mkdir(notebook_dir)
-    except OSError as e:
-        if e.errno != errno.EEXIST or not os.path.isdir(notebook_dir):
-            raise
+def make_all_sessions(exercise_dir, helpers_dir, notebook_dir):
+    dirty = subprocess.check_output(['git', '-C', helpers_dir, 'clean', '-nxd'])
+    if len(dirty.strip()) > 0:
+        print('Please clean up {} and run again.'.format(os.path.relpath(helpers_dir)))
+        print('(E.g. with git -C {} clean -fxd.)'.format(os.path.relpath(helpers_dir)))
+        print(dirty)
+        sys.exit(1)
 
-    def walk(_, path_dir, names):
+    shutil.rmtree(notebook_dir, True)
+    os.mkdir(notebook_dir)
+
+    def make(_, path_dir, names):
         if 'instructions.md' in names:
             part = os.path.basename(path_dir).partition('part')
             session = os.path.basename(os.path.dirname(path_dir)).partition('session')
             assert(part[0] == '' and session[0] == '')
             notebook_name = 'Session {} Part {}.ipynb'.format(session[2], part[2])
             make_session_notebook(path_dir, os.path.join(notebook_dir, notebook_name))
-    os.path.walk(source_dir, walk, None)
+    os.path.walk(exercise_dir, make, None)
+
+    for name in os.listdir(helpers_dir):
+        src_dir = os.path.join(helpers_dir, name)
+        dst_dir = os.path.join(notebook_dir, name)
+        if os.path.isdir(src_dir):
+            shutil.copytree(src_dir, dst_dir)
 
 if __name__ == '__main__':
     root_dir = os.path.dirname(os.path.realpath(__file__))
     exercises_dir = os.path.join(root_dir, 'regent', 'exercises')
+    helpers_dir = os.path.join(root_dir, 'regent', 'helpers')
     notebook_dir = os.path.join(root_dir, 'regent', 'notebooks')
-    make_all_sessions(exercises_dir, notebook_dir)
+    make_all_sessions(exercises_dir, helpers_dir, notebook_dir)
